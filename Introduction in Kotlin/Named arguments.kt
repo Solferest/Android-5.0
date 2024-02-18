0fce60e6f7eed3807d14d10efcab5511d8f6cfcd
Solferest <112902148+Solferest@users.noreply.github.com>
2024-02-18 16:21:31 +0300

    Update and rename Named arguments.kt to Classes.kt

--- Introduction in Kotlin/Named arguments.kt
+++
@@ -1,8 +0,0 @@
-fun joinOptions(options: Collection<String>) =
-    options.joinToString(prefix = "[", postfix = "]", separator = ", ")
-
-fun main() {
-    val options = listOf("a", "b", "c")
-    val result = joinOptions(options)
-    println(result)
-}
